f4004292e1ceafa3cbb4f514e5cff868f84ccf78
Mohamed Alaa <ma722072207220@gmail.com>
2023-06-04 19:00:26 +0300

    updated

--- project/urls.py
+++ project/urls.py
@@ -8,6 +8,7 @@ from django.conf import settings
 urlpatterns = [
     path('silk/', include('silk.urls', namespace='silk')),
     path('admin/', admin.site.urls),
+    path('api/auth/', include('users.urls')),
     path('api/room/', include('rooms.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('__debug__/', include('debug_toolbar.urls')),
--- users/views.py
+++ users/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework import views, status
+from django.contrib.auth import authenticate
+from rest_framework.response import Response
+from .serializers import UserSerializer
+from rest_framework.authtoken.models import Token
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+
+# ----- Login ------
+class LoginView(views.APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            token, create = Token.objects.get_or_create(user=user)
+            return Response({
+                "message": "Login Successfully",
+                'data': UserSerializer(user).data,
+                "token": token.key
+            })
+        else:
+            return Response({'message': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
